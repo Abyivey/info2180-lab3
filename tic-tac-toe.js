fa6df690b89e25acbf3a4c4e0fe5d214e8d85060
Abygayle Ivey <abyivey@gmail.com>
2020-10-11 16:13:20 -0500

    Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -13,5 +13,36 @@ window.onload = function(){
 
     BoardMod();
     console.log(board);
+    board.addEventListener("click",Player);
     
 }
+
+function Player(e){
+
+
+
+    if(e.target.nodeName == "DIV"){
+        if(state[0] == ""){
+            state[0] = "X";
+            e.target.innerHTML = "X";
+            e.target.classList.add("X");
+        }else if(state[counter] == "O" && e.target.innerHTML == "")
+        {
+            state.push("X")
+            counter--;
+            e.target.innerHTML = "X";
+            e.target.classList.add("X");
+        
+        }
+        else if(state[counter] == "X" && e.target.innerHTML == "")
+        {
+            state.push("O");
+            counter++;
+            e.target.innerHTML = "O";
+            e.target.classList.add("O");
+        }
+    }
+}
+
+    
+
