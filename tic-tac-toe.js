aa22488e428d5a411ff04ce6738d90db6e64028b
Abygayle Ivey <abyivey@gmail.com>
2020-10-11 03:27:39 -0500

    Exercise 1 update

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,15 +2,16 @@ var state =[""];
 var counter =0;
 function BoardMod(){
     var board = document.getElementById("board")
-    var boardSect = board.getElementsByTagName("div");
-    for(i=0; i<boardSect.length;i++){
-        boardSect[i].className = "square";
+    var boardSections = board.getElementsByTagName("div");
+    for(i=0; i<boardSections.length;i++){
+        boardSections[i].className = "square";
     }
 
 }
+
 window.onload = function(){
 
-    boardSetup();
+    BoardMod();
     console.log(board);
     
 }
